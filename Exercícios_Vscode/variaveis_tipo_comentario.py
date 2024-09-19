59074cbcfb7f98a8f7e99090f0a069e4edf87497
bottinn <Bottinliuv@gmail.com>
2024-09-19 15:17:58 -0300

    COMMIT 19/09
    Inicio dos exercícios que serão realizados em aula, atividade feita até o número 4.

---
+++ Exercícios_Vscode/variaveis_tipo_comentario.py
@@ -0,0 +1,13 @@
+#teste de comentário!!!(somente uma linha)
+'''comentário que pode passar de uma linha
+                    :)
+'''
+    
+print("Boa tarde pessoal da disciplina de Programação II.")                
+
+nome = 'joão'
+numero = 1,20
+idade = 11
+
+int(numero)
+
