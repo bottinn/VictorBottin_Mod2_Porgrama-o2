ed74b5267696987123c18392a720ff8a4ad96c64
bottinn <Bottinluv@gmail.com>
2024-09-25 16:32:53 -0300

    COMMIT 25/09
    Exercícios feitos em aula

--- Exercícios_Vscode/variaveis_tipo_comentario.py
+++ Exercícios_Vscode/variaveis_tipo_comentario.py
@@ -1,13 +1,60 @@
-#teste de comentário!!!(somente uma linha)
-'''comentário que pode passar de uma linha
-                    :)
-'''
-    
-print("Boa tarde pessoal da disciplina de Programação II.")                
+                #exercício 1 
+# #teste de comentário!!!(somente uma linha)
 
-nome = 'joão'
+
+
+                #Exercício 2 
+'''comentário que pode passar de uma linha
+                    :)  '''
+                      
+                #Exercício 3    
+print("Boa tarde pessoal da disciplina de Programação II.")    
+
+                #Exercício 4 
+nome = 'joao'
 numero = 1,20
 idade = 11
 
-int(numero)
+                #Exercício 5  
+idade = 15 
+print('A idade da pessoa é de', idade)
+
+numero = 30
+print('o número escolhido é', numero)
+
+nome = 'joão'
+print('o nome da pessoa é ', nome)
+
+
+                #exercício 6 
+preco = int(input("insira o valor"))
+print('o valor é:', preco)
+
+                #Exercício 7 
+babuino = 4
+macaco = 2
+soma = macaco + babuino
+print(f"{macaco} mais {babuino} é igual a {soma}")
+
+                #Exercício 8 
+cachorro = 20
+gato = 10
+subtracao = gato - cachorro
+print(f"{gato} menos {cachorro} é igual a {subtracao}")     
+                
+                #exercício 9
+peru = 23
+pintinho = 3
+multiplicacao = peru * pintinho
+print(f"{peru} vezes {pintinho} é igual a {multiplicacao}")
+
+                #Exercício 10           
+
+peixe = 12
+palhaco = 3
+divisao = peixe / palhaco
+print(f"{peixe} dividido por {palhaco} é igual a {divisao}")
+                
+
+                
 
