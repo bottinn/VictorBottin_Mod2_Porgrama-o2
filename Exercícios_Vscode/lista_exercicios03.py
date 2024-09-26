6714943f15a8dc2db14fcb51c97111f53c328fbf
bottinn <Bottinluv@gmail.com>
2024-09-26 15:07:14 -0300

    COMMIT 26/09
    Exercícios que estão sendo realizados em sala

---
+++ Exercícios_Vscode/lista_exercicios03.py
@@ -0,0 +1,27 @@
+#1
+#Num_1 = int(input("Insira um valor numérico:  "))
+#Num_2 = int(input("Insira o segundo valor: "))
+
+#Soma = Num_1 + Num_2
+#subtracao = Num_1 - Num_2
+#multiplicacao = Num_1 * Num_2
+#divisao = Num_1 / Num_2
+
+#print(f"A soma desses números é de: {Soma}")
+#print(f"A subtração desses números é de: {subtracao}")
+#print(f"A multiplicação desses números é de: {multiplicacao}")
+#print(f"A divisão desses números é de: {divisao}")
+
+#2
+
+#Peso = float(input("Insira o seu peço em Quilos: "))
+#altura = float(input("insira a sua altura em metros: "))
+#IMC = Peso / (altura*altura)
+#print(f"Seu IMC é de {IMC}")
+
+#3
+
+Frase = "Bem vindo turma da Programação II ao mundo da programação Python!!!" [:: - 1]
+#print(Frase)
+
+#4
