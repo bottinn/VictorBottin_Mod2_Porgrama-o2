20c2cb721cfd75a48c1d5bab36cd271f3564007e
bottinn <Bottinnluv@gmail.com>
2024-11-21 14:46:55 -0200

    COMMIT 21/11
    Finalização de exercícios Função

--- Funções/Tratamento_Strings/Exercícios_Strings.py
+++ Funções/Tratamento_Strings/Exercícios_Strings.py
@@ -63,22 +63,82 @@ def FuncaoVerdadeiroFalso(FraseParaTerminar,FraseOuPalavraParaVerificar):
 (FuncaoVerdadeiroFalso(FraseParaTerminar, FraseOuPalavraParaVerificar))
 
 # 5. Crie uma função que implemente a questão 1 da avaliação01 teórico-prática.
-
+def conceitoVariavel():
+    print('''Em Python, uma variável é um nome que se refere a um valor específico. Pense nela como uma caixinha onde você pode guardar um número, um texto, uma lista ou qualquer outro tipo de dado. Você dá um nome a essa caixinha para que possa encontrar o valor armazenado mais tarde.
+    Existem algumas regras e convenções que você deve seguir ao nomear suas variáveis em Python:
+    Os nomes das variáveis devem começar com uma letra ou um underscore (_).
+    Os nomes das variáveis podem conter letras, números e underscores.
+    Os nomes das variáveis não podem conter espaços ou caracteres especiais, e não podem ser palavras reservadas do Python (como if, for, print, etc.).
+    Python diferencia maiúsculas de minúsculas, portanto variavel e Variavel seriam consideradas variáveis diferentes.''')
+def conceitoLacoInfinito():
+    print('''O loop infinito é um conceito importante na programação, e em Python não é diferente. Em termos simples, um loop infinito é um trecho de código que se repete continuamente, sem uma condição de parada definida. Isso significa que o código dentro do loop será executado repetidamente, até que uma condição específica seja atendida para interromper a execução. Em Python, podemos criar um loop infinito usando a estrutura de controle “while True”.''')
+def conceitoSemantica():
+    print('''O Que é Semântica? Aqui, a semântica é clara: o código soma x e y e imprime o resultado, que será 15 . Se você trocasse o + por - , o resultado seria -5 . A semântica ajuda você a entender o que cada linha do código está realmente fazendo. É a parte abstrata do código.''')
+def conceitoSintaxe():
+    print('''A sintaxe é a parte "concreta" do código, as palavras e comandos utilizados. Ela checa se eles estão escritos da forma correta perante as regras de cada um e se funcionam dentro de seu contexto.''')
+def inputDoUsuario():
+    while True:
+        opcaoUsuario = input("Insira a opção que deseja escolher: ").strip().lower()
+        if (opcaoUsuario == ""):
+            print("Insira, pelo menos, ALGUMA COISA.")
+        elif (opcaoUsuario == "sair"):
+            break
+        elif (opcaoUsuario == "1"):
+            conceitoVariavel()
+            break
+        elif (opcaoUsuario == "2"):
+            conceitoLacoInfinito()
+            break
+        elif (opcaoUsuario == "3"):
+            conceitoSemantica()
+            break
+        elif (opcaoUsuario == "4"):
+            conceitoSintaxe()
+            break
+        elif (opcaoUsuario.isdigit == True):
+            print("Insira um número disponível no menu.")
+        else:
+            print("Para sair do programa, digite 'sair'.")
+print('''Para cada, opção, digite seu número correspondente:
+      1 - Conceito de variáveis
+      2 - Conceito de loops infinitos 
+      3 - Conceito de semântica
+      4 - Conceito de sintaxe''')
+inputDoUsuario()
 
 
 # 6. Crie uma função que implemente a questão 4 da avaliação01 teórico-prática.
-CaracteresMinimo = 6
-CaracteresMaximo = 20
-LetraMinuscula = SenhaParaSerFeita
-CaracteresEspecial = r"!@#$%&*?<>:}{[]"
-
-
-SenhaParaSerFeita = input("Insira a senha que deseja criar, ela deve ter no mínimo 6 carácteres, e no maximo 6, \numa letra maiuscula e miscula, com um caracter especial (#@$%):   \n")
-def RequisitosSenha(CarateresMinimo,CaracteresMaximo,LetraMinuscula,LetraMaiscula,CaracterEspecial):
-    if SenhaParaSerFeita >= CaracteresMinimo and SenhaParaSerFeita <= CaracteresMaximo:
-        print()
-
-print(CaracteresEspecial)
+('''- Uma letra maiúscula;
+- Uma letra minúscula;
+- Um caracter especial ($#!@&)
+- Mínimo de 6 caracteres;
+- Máximo de 20 caracteres.''')
+def criarSenha():
+    while True:
+        senhaOriginal = input("Insira a senha:\n").strip()
+        if (len(senhaOriginal) < 6):
+            print("A senha deve ter mais de 6 caracteres.")
+        elif (len(senhaOriginal) > 20):
+            print("A senha deve ter menos de 20 caracteres.")
+        elif (not any(i.isupper() for i in senhaOriginal)):
+            print("A senha deve conter pelo menos uma letra maiúscula.")
+        elif (not any(i.islower() for i in senhaOriginal)):
+            print("A senha deve conter pelo menos uma letra minúscula.")
+        elif (not any(i in "$#!@&" for i in senhaOriginal)):
+            print("A senha deve conter pelo menos um caracter especial ($#!@&).")
+        else:
+            print("Senha criada com sucesso! (espaços retirados!)")
+            return senhaOriginal
+def verificarSenha(senhaOriginal):
+    while True:
+        tentativaConfirmacao = input("Confirme a senha criada:\n")
+        if (tentativaConfirmacao != senhaOriginal):
+            print("A senha inserida não está correta. Tente novamente.")
+        else:
+            print("Senha confirmada com sucesso!")
+            break
+senha = criarSenha()
+verificarSenha(senha)
 
 # 7. Crie uma função que receba uma String, toda em letras minúsculas, e retorne essa
 # mesma String passada como parâmetro com a primeira letra de cada palavra em
